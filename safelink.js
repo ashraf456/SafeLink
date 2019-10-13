78843c9645bc67223390fc824e21a6ac5354ffb2
ashraf456 <55910049+ashraf456@users.noreply.github.com>
2019-10-13 22:59:22 +0530

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="blog-post_28.html"}
 if(x=="2"){xxx="download-any-movies-and-tv-shows-in-one.html"}
 if(x=="3"){xxx="top-5-way-to-earn-money-online.html"}
-if(x=="4"){xxx="everything-is-fine-life-is-not-defined.html"}a_to_ve[i].href="https://href.li/?https://aiworld4u.blogspot.com/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="everything-is-fine-life-is-not-defined.html"}a_to_ve[i].href="https://href.li/?https://mkv300hd.blogspot.com/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
