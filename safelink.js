6487010f7c0766a250727e12096074d07fc3bf12
ashraf456 <55910049+ashraf456@users.noreply.github.com>
2019-10-31 20:53:11 +0530

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="blog-post_28.html"}
+if(x=="1"){xxx="how-to-use-virtual-root-app-vmos.html"}
 if(x=="2"){xxx="download-any-movies-and-tv-shows-in-one.html"}
-if(x=="3"){xxx="top-5-way-to-earn-money-online.html"}
-if(x=="4"){xxx="everything-is-fine-life-is-not-defined.html"}a_to_ve[i].href="https://href.li/?https://mkv300hd.blogspot.com/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="xposed-installer-in-android-no-root.html"}
+if(x=="4"){xxx="what-is-server-and-how-does-it-works.html"}a_to_ve[i].href="https://href.li/?https://mkv300hd.blogspot.com/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
